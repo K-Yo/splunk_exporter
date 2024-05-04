a2f07dd047e9780dc03ae4b9a2f2613c7da96bba
K-Yo <nobody@localhost>
2024-05-04 21:36:04 +0200

    🗑️ 🚚 name properly exporter

--- config/config.go
+++ config/config.go
@@ -32,13 +32,13 @@ type SafeConfig struct {
 
 func NewSafeConfig(reg prometheus.Registerer) *SafeConfig {
 	configReloadSuccess := promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-		Namespace: "blackbox_exporter",
+		Namespace: "splunk_exporter",
 		Name:      "config_last_reload_successful",
-		Help:      "Blackbox exporter config loaded successfully.",
+		Help:      "Splunk exporter config loaded successfully.",
 	})
 
 	configReloadSeconds := promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-		Namespace: "blackbox_exporter",
+		Namespace: "splunk_exporter",
 		Name:      "config_last_reload_success_timestamp_seconds",
 		Help:      "Timestamp of the last successful configuration reload.",
 	})
