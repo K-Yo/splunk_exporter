f559f03c8d0ed6b16ecdc5d527dec7bda3e0cc97
K-Yo <nobody@localhost>
2024-10-18 18:27:21 +0200

    ✨ log instance_id when getting deployment health metrics

--- exporter/health_manager.go
+++ exporter/health_manager.go
@@ -2,6 +2,7 @@ package exporter
 
 import (
 	"fmt"
+	"strings"
 
 	splunklib "github.com/K-Yo/splunk_exporter/splunk"
 	"github.com/go-kit/log"
@@ -33,7 +34,7 @@ func newHealthManager(namespace string, spk *splunklib.Splunk, logger log.Logger
 		deploymentDescriptor: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "health", "deployment"),
 			"Splunk exported metric from deployment health API",
-			[]string{"name"}, nil,
+			[]string{"name", "instance_id"}, nil,
 		),
 	}
 
@@ -139,8 +140,17 @@ func (hm *HealthManager) getMetricsDeployment(ch chan<- prometheus.Metric, path 
 	}
 	level.Debug(hm.logger).Log("num_red", num_red, "num_yellow", num_yellow)
 
+	skipMetric := false
+	level.Debug(hm.logger).Log("path", path, "lenpath", len(path))
+	skipMetric = skipMetric || disabled     // ignore disabled metrics
+	skipMetric = skipMetric || health == "" // ignore when we cannot parse health
+
+	// ignore when it’s a metric ending with "/instances"
+	if len(path) > 10 {
+		skipMetric = skipMetric || (path[len(path)-10:] == "/instances")
+	}
 	// Add current metric
-	if !disabled && health != "" {
+	if !skipMetric {
 		healthValue, err := hm.healthToFloat(health)
 		if err != nil {
 			level.Error(hm.logger).Log("msg", "Cannot get metrics because of unknown health value", "path", path, "err", err)
@@ -151,8 +161,18 @@ func (hm *HealthManager) getMetricsDeployment(ch chan<- prometheus.Metric, path 
 			displayPath = "/"
 		}
 
+		// process when metrics are in the form of /splunkd/resource_usage/iowait/sum_top3_cpu_percs__max_last_3m/instances/8F8096AF-A456-4974-92FB-966103FA9752
+		instanceId := ""
+		if strings.Contains(path, "/instances/") {
+			parts := strings.Split(path, "/")
+			if parts[len(parts)-2] == "instances" {
+				instanceId = parts[len(parts)-1]
+			}
+			displayPath = strings.Join(parts[:len(parts)-2], "/")
+		}
+
 		ch <- prometheus.MustNewConstMetric(
-			hm.deploymentDescriptor, prometheus.GaugeValue, healthValue, displayPath,
+			hm.deploymentDescriptor, prometheus.GaugeValue, healthValue, displayPath, instanceId,
 		)
 	}
 
