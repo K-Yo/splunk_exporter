dfd2d2e5798ea3109adfcb8ef1669fef393a037b
K-Yo <nobody@localhost>
2024-10-18 15:23:26 +0200

    ✅ tests for password-based authentication

--- config/config_test.go
+++ config/config_test.go
@@ -6,10 +6,43 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func TestLoadConfig(t *testing.T) {
+// TestLoadConfigToken
+// Given
+//
+//	A valid config file using a token
+//
+// When
+//
+//	reloading the config
+//
+// Then
+//
+//	Config reload happens without error
+func TestLoadConfigToken(t *testing.T) {
 	sc := NewSafeConfig(prometheus.NewRegistry())
 
-	err := sc.ReloadConfig("testdata/splunk_exporter-good.yml", nil)
+	err := sc.ReloadConfig("testdata/splunk_exporter-token-good.yml", nil)
+	if err != nil {
+		t.Errorf("Error loading config %v: %v", "splunk_exporter-good.yml", err)
+	}
+}
+
+// TestLoadConfigUser
+// Given
+//
+//	A valid config file using a username and password
+//
+// When
+//
+//	reloading the config
+//
+// Then
+//
+//	Config reload happens without error
+func TestLoadConfigUser(t *testing.T) {
+	sc := NewSafeConfig(prometheus.NewRegistry())
+
+	err := sc.ReloadConfig("testdata/splunk_exporter-user-good.yml", nil)
 	if err != nil {
 		t.Errorf("Error loading config %v: %v", "splunk_exporter-good.yml", err)
 	}
