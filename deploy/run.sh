e411817cdffba98ce096d5624a87e4eed821ad4c
K-Yo <nobody@localhost>
2024-10-18 17:32:29 +0200

    test bench with DMC

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,9 +6,11 @@ set -e
 set -v
 
 # Start the stack
+export SPLUNK_IMAGE="splunk/splunk:9.2"
+docker run --rm -it ${SPLUNK_IMAGE:-splunk/splunk:latest} create-defaults > default.yml
 docker compose up -d --remove-orphans 
 
 # Please wait for Splunk to be initialized, check this with the command:
-# docker compose logs sh1 -f
+# docker compose logs dmc -f
 # If you need to reload config, you may use the following command:
 # curl -X POST http://localhost:9115/-/reload
