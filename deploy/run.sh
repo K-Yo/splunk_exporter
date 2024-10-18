8231143e117f569c84f6166da2648316449bd66a
K-Yo <nobody@localhost>
2024-10-18 15:40:50 +0200

    use username/password for test bench

--- deploy/run.sh
+++ deploy/run.sh
@@ -5,19 +5,10 @@ set -e
 # print commands
 set -v
 
-# initiate conf file
-touch ./splunk_exporter.yml
-
 # Start the stack
-docker compose up -d prometheus grafana splunk
+docker compose up -d --remove-orphans 
 
-# Wait for splunk to be initialized
-until docker logs -n1 splunk 2>/dev/null | grep -q -m 1 '^Ansible playbook complete'; do sleep 0.2; done
-
-# Generate api key
-export SPLUNK_TOKEN=$(curl -k -u admin:splunkadmin -X POST https://localhost:8089/services/authorization/tokens?output_mode=json --data name=admin --data audience=splunk_exporter | jq -r '.entry[0].content.token')
-cat splunk_exporter.yml.src | envsubst > splunk_exporter.yml
-
-# start splunk_exporter
-docker compose up -d
+# Please wait for Splunk to be initialized, check this with the command:
+# docker compose logs sh1 -f
+# If you need to reload config, you may use the following command:
 # curl -X POST http://localhost:9115/-/reload
