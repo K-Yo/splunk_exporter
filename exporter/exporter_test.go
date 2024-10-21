eaa2966b3a60a7a6183a1a19e974c9890577d97d
K-Yo <nobody@localhost>
2024-10-21 11:30:07 +0200

    refacto some tests

---
+++ exporter/exporter_test.go
@@ -0,0 +1,4 @@
+package exporter
+
+// We shall test properly metrics being collected (from testdata/deploymenthealth.json for example).
+// We’re waiting for https://github.com/prometheus/client_golang/issues/1639 to be resolved for this.
